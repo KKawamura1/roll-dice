6cbb80c3cf0968b21f8b0d025d852c488d76ca07
Keigo Kawamura <kkawamura@preferred.jp>
2019-08-08 01:55:24 +0900

    Update to use readline

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-import argparse
 from roll_dice.query import Query
 
 
-def main(arg: str = None) -> None:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('target', type=str)
-    if arg is not None:
-        params = parser.parse_args(arg.split())
-    else:
-        params = parser.parse_args()
-    target: str = params.target
-    print(target)
-    result = Query.parse_as_query(target).roll()
-    print(str(result))
+def main() -> None:
+    # Completion
+    import readline
+
+    while True:
+        try:
+            line = input('Input: ')
+        except EOFError:
+            break
+        result = Query.parse_as_query(line).roll()
+        print(str(result))
 
 
 if __name__ == "__main__":
